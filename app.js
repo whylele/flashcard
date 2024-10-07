8596d7e8582920e250b4cf0ec37ac37227df1997
whylele <163873282+whylele@users.noreply.github.com>
2024-10-07 11:33:34 -0300

    .

---
+++ app.js
@@ -0,0 +1,16 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.createElement('article');
+    cartao.className = 'cartao';
+}
+
+cartao.innerHTML = 
+<div class="cartao_conteudo">
+        <h3>Programação</h3>
+        <div class="cartao_conteudo_pergunta">
+           <p>O que é JavaScript</p> 
+        </div>
+     <div class="cartao_conteudo_resposta">
+       <p>O JavaScript é uma linguagem de programação</p> 
+        </div>
+        </div>
---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Hello Kitty',
+    'Qual è a altura da Hello Kitty',
+    'equivalente a 5 maças empilhadas',
+)
+
+criaCartao(
+    'My Melody',
+    'Quem é'My Melody?,
+    'A My Melody nasceu na floresta de Mary Land. Ela é super legal e faz amizade facilmente. Sua marca registrada é um lindo capuz, que sua avó costurou especialmente para ela.',
+)
+
+criaCartao(
+    'Kuromi',
+    'Pergunta',
+    'Resposta',
+)
+
+criaCartao(
+    'Cinnamoroll',
+    'Pergunta',
+    'Resposta',
+)
+
+criaCartao(
+    'Pompompurin',
+    'Quem é Pompompurin?',
+    'Pompompurin é um tranquilo filhotinho Golden Retriever que tem um enorme talento para cochilar. Ele ama a frase “vamos sair”, tanto quanto odeia a palavra “ficar”. Faz amizade com todos que encontra, e tem um sonho de ser realmente grande. Seu hobby é colecionar os sapatos de seus donos e tem seu próprio esconderijo secreto.',
+)
+
+criaCartao(
+    'Keroppi',
+    'Pergunta',
+    'Resposta',
+)
