ab4e3c14c1dc5ec3cf002bce6193f1fa3384cc3d
whylele <163873282+whylele@users.noreply.github.com>
2024-10-16 11:40:45 -0300

    .

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ function criaCartao(categoria, pergunta, resposta) {
 
 
 cartao.innerHTML = `
-<div class="cartao_conteudo">
+<div class="cartao__conteudo">
         <h3>${categoria}</h3>
-        <div class="cartao_conteudo_pergunta">
+        <div class="cartao__conteudo__pergunta">
            <p>${pergunta}</p> 
         </div>
-     <div class="cartao_conteudo_resposta">
+     <div class="cartao__conteudo__resposta">
        <p>${resposta}</p> 
         </div>
         </div>
